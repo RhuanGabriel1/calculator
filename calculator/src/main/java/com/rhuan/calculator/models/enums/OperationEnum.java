c1a71239ec559680821578645587b8fd3ff6fc88
Rhuan Martins <74109343+RhuanGabriel1@users.noreply.github.com>
2025-03-01 01:24:14 -0300

    feat: Calculator Body Addition

---
+++ calculator/src/main/java/com/rhuan/calculator/models/enums/OperationEnum.java
@@ -0,0 +1,10 @@
+package com.rhuan.calculator.models.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum OperationEnum {
+    SUM, SUBTRACT, MULTIPLY, DIVIDE;
+}
---
+++ calculator/src/main/java/com/rhuan/calculator/services/CalculatorService.java
@@ -0,0 +1,20 @@
+package com.rhuan.calculator.services;
+
+import com.rhuan.calculator.models.enums.OperationEnum;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CalculatorService {
+
+    public double calculate(double num1, double num2, OperationEnum operationEnum){
+        return  switch (operationEnum){
+            case SUM -> num1 + num2;
+            case SUBTRACT -> num1 - num2;
+            case MULTIPLY -> num1 * num2;
+            case DIVIDE -> num1 / num2;
+            default -> 0;
+        };
+    }
+}
